feb9bd72e868a943834870886d03e56512b3c78e
trucdp <trucpham939.k44@st.ueh.edu.vn>
2022-02-14 10:23:12 +0700

    add file py

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Datasets-of-Customer-Reviews
-The dataset consists of more than 100.000 customer reviews from Shopee websites in categories: Clothing, Shoes, Bags, Luggage, Watches, and other Fashion accessories.
+## About the traning dataset
+The traning dataset consists of more than 1000 customer reviews from Vietname e-commerce websites in categories: Clothing, Shoes, Bags, Luggage, Watches, and other Fashion accessories.
+
+It were crawled and labeled        
+
+The data can be downloaded [here](https://drive.google.com/file/d/1JlZpgM5uBZI-xeayv-AnXnW_OQjzaUYP/view?usp=sharing).
+ 
+#   About 100k reviews 
+
+## How were the training data prepared
+The training data have been processed through following steps:
+
+  1. Customer reviews from e-commerce websites were crawled. The crawled data include **reviews** (text) and **rating** (integers 1-5 corresponding to 1-5 rating stars).
+  
+  2. The **rating** data were converted into 3 class labels:
+  
+    1-2 stars => class label 0 (negative) 
+    4-5 stars => class label 1 (positive) 
+    3 stars   => class label 2 (neutral)
+   
+  3. The **class labels** were then manually reassigned. 
+
+The dataset is saved in *utf-8 csv* format with 2 columns: **label**, **review**. There are about 100.000 reviews in the training data.
+
+
+
+
---
+++ datasets/crawl_customer_reviews_shoppe.py
@@ -0,0 +1,41 @@
+import re
+import json
+import requests
+import pandas as pd
+
+
+url = "https://shopee.vn/Qu%E1%BA%A7n-short-%C4%91%C3%B9i-n%E1%BB%AF-c%E1%BA%A1p-cao-ch%E1%BA%A5t-tuy%E1%BA%BFt-m%C6%B0a-i.290065942.6244070348?sp_atk=f3ad5a0f-3ee2-431a-9529-3b00499a0445"
+
+r = re.search(r"i\.(\d+)\.(\d+)", url)
+shop_id, item_id = r[1], r[2]
+ratings_url = "https://shopee.vn/api/v2/item/get_ratings?filter=0&flag=1&itemid={item_id}&limit=20&offset={offset}&shopid={shop_id}&type=0"
+
+offset = 0
+d = {"username": [], "rating": [], "comment": []}
+while True:
+    data = requests.get(
+        ratings_url.format(shop_id=shop_id, item_id=item_id, offset=offset)
+    ).json()
+
+    # uncomment this to print all data:
+    # print(json.dumps(data, indent=4))
+    # leng enumerate tra ket qua duoi dang liet ke
+    i = 1
+    for i, rating in enumerate(data["data"]["ratings"], 1):
+        d["username"].append(rating["author_username"])
+        d["rating"].append(rating["rating_star"])
+        d["comment"].append(rating["comment"])
+
+        print(rating["author_username"])
+        print(rating["rating_star"])
+        print(rating["comment"])
+        print("-" * 100)
+
+    if i % 20:
+        break
+
+    offset += 20
+
+    df = pd.DataFrame(d)
+    print(df)
+    df.to_csv("./datasets/data.csv", index=False)
